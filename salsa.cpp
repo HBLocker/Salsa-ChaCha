e749046c433fe8f98eecb032c2a327770ead33a3
prettyDamCool <48737752+prettyDamCool@users.noreply.github.com>
2020-11-28 12:26:41 +0000

    Add files via upload
    An example of SALSA20, comments in the code show shite papers I have read to gain an understanding of the algorithm implemented. 
    Then CHACHA was added after SALSA correctly compiled, this was an instance of changing how the rounds worked.

---
+++ salsa.cpp
@@ -0,0 +1,74 @@
+// based from the follwing paper
+//https://pdfs.semanticscholar.org/3ebf/2fe53c0b245520b8b4da37996fb895de3c79.pdf
+//https://cr.yp.to/snuffle/design.pdf
+//slaa20 256-bit stream cipher alg
+//salsa contains three main functions:
+// 32 bit addition = a+b mod^32
+//https://en.wikipedia.org/wiki/Salsa20
+//https://cr.yp.to/salsa20.html
+//https://eprint.iacr.org/2007/472.pdf
+#include <stdio.h>
+#include <stdint.h>
+//papers denoted rotation in section
+#define Rotation(x,n) (((x) << (n)) | ((x) >> (32-(n))))
+//rotation  https://stackoverflow.com/questions/29676648/explaining-method-x-n-x-32-n
+#define quarter(a,b,c,d) do {\
+	b ^= Rotation(d+a, 7);\
+	c ^= Rotation(a+b, 9);\
+	d ^= Rotation(b+c, 13);\
+	a ^= Rotation(c+d, 18);\
+} while (0)
+//taken from https://cr.yp.to/salsa20.html
+void main_salasa_word(uint32_t out[16],uint32_t const in[16])
+{
+	uint32_t x[4][4];
+	int i;
+	for (i=0; i<16; ++i) x[i/4][i%4] = in[i];
+	for (i=0; i<10; ++i)
+	{
+		//20 rounds
+		quarter(x[0][0], x[1][0], x[2][0], x[3][0]); //even values
+		quarter(x[1][1], x[2][1], x[3][1], x[0][1]);
+		quarter(x[2][2], x[3][2], x[0][2], x[1][2]);
+		quarter(x[3][3], x[0][3], x[1][3], x[2][3]);
+		//////////////////////////////////////////////
+		quarter(x[0][0], x[0][1], x[0][2], x[0][3]); 	// odd values
+		quarter(x[1][1], x[1][2], x[1][3], x[1][0]);
+		quarter(x[2][2], x[2][3], x[2][0], x[2][1]);
+		quarter(x[3][3], x[3][0], x[3][1], x[3][2]);
+	}
+	for (i=0; i<16; ++i) out[i] = x[i/4][i%4] + in[i];
+}
+//transformation fothe block mentioend in https://eprint.iacr.org/2007/472.pdf
+//salsa takes in 32bit words, and a 256bit key,64bit nonce value.
+void salsa20_block(uint8_t *out, uint8_t key[32], uint64_t nonce, uint64_t index)
+{
+	//stattic const to stop change of vlaue.
+	static const char c[16] = "filler";
+	//an attempt at the "quarterround" function, which is used for SALSA20
+	//bellow is a shit to the right for the values
+	#define LE(p) ( (p)[0] | ((p)[1]<<8) | ((p)[2]<<16) | ((p)[3]<<24) )
+	uint32_t in[16] = {LE(c),LE(key),LE(key+4), LE(key+8),LE(key+12),LE(c+4),nonce&0xff,
+  nonce>>32, index&0xff, index>>32,LE(c+8),LE(key+16),LE(key+20),LE(key+24), LE(key+28),LE(c+12)};
+	uint32_t wordout[16];
+	main_salasa_word(wordout, in);
+	int i;
+	for (i=0; i<64; ++i) out[i] = 0xff & (wordout[i/4] >> (8*(i%4)));
+}
+void salsa20_main_function(uint8_t *message, uint64_t mlen, uint8_t key[32], uint64_t nonce)
+{
+	int i;
+	uint8_t block[64];
+	for (i=0; i<mlen; i++) {
+		if (i%64 == 0) salsa20_block(block, key, nonce, i/64);
+		message[i] ^= block[i%64]; //64 bit shift is here
+	}
+}
+int  main () {
+	uint8_t key[32] = {0};
+	uint64_t nonce = 0;
+	uint8_t msg[64] = {0};
+
+	salsa20_main_function(msg, sizeof(msg), key, nonce);
+	int i; for (i=0; i<sizeof(msg); ++i) printf("%02X", msg[i]); printf("\n");
+}
